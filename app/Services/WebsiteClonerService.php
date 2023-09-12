18284018c19c23ef137bcc5a259b7be023a23733
Douglas Lopes <dl.aguiar@yahoo.com.br>
2023-09-12 14:08:39 -0300

    Update WebsiteClonerService.php

--- app/Services/WebsiteClonerService.php
+++ app/Services/WebsiteClonerService.php
@@ -2,12 +2,14 @@
 
 namespace App\Services;
 
-use GuzzleHttp\Client;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Http;
 use Symfony\Component\DomCrawler\Crawler;
 
 class WebsiteClonerService
 {
     private $output_dir;
+    private $targetUrl;
 
     public function __construct($output_dir)
     {
@@ -16,30 +18,22 @@ class WebsiteClonerService
 
     public function clone($targetUrl): void
     {
-        $response = (new Client())->get($targetUrl);
-
-        if (
-            $response->getStatusCode() < 200
-            || $response->getStatusCode() >= 300
-        ) {
-            throw new \Exception('Failed to clone website');
-        }
-
-        $html = (string) $response->getBody();
-        $crawler = new Crawler($html);
+        $this->targetUrl = $targetUrl;
+        $html = $this->getBody($targetUrl);
 
         if (!is_dir($this->output_dir)) {
             mkdir($this->output_dir, 0755, true);
         }
 
-        parse_url($targetUrl, PHP_URL_SCHEME) . '://' . parse_url($targetUrl, PHP_URL_HOST);
+        $crawler = new Crawler($html);
 
         $crawler->filter('link[rel="stylesheet"], script[src], img[src]')
             ->each(function ($node) use (&$html) {
                 $url = $node->attr('href') ?: $node->attr('src');
                 $path = parse_url($url, PHP_URL_PATH);
                 $filename = $this->output_dir . $path;
-                $contents = file_get_contents($url);
+
+                $contents = $this->getContent($url);
 
                 if (!is_dir(dirname($filename))) {
                     mkdir(dirname($filename), 0755, true);
@@ -52,5 +46,39 @@ class WebsiteClonerService
             });
 
         file_put_contents($this->output_dir . '/index.html', $html);
+    }
+
+    private function getBody($url): string
+    {
+        $response = Http::withHeaders([
+            'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
+            'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+        ])->get($url);
+
+        if (!$response->successful()) {
+            throw new \Exception('Failed to clone website');
+        }
+
+        return $response->body();
+    }
+
+    private function getContent($url): string
+    {
+        if (Str::startsWith($url, '//')) {
+            $url = str_replace('//', '', $url);
+            $url = 'https://' . $url;
+        }
+
+        if (Str::startsWith($url, '/')) {
+            $host = parse_url($this->targetUrl, PHP_URL_HOST);
+            $url = $host . $url;
+        }
+
+        $response = Http::withHeaders([
+            'user-agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36',
+            'accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+        ])->get($url);
+
+        return $response->body();
     }
 }
